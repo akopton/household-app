0ff07b3002f9bb9d6e8fdfb0683e3c012a4d6f95
Aleksander Kopton <kopton.olek@gmail.com>
2025-12-12 00:53:44 +0100

    feat: add register button

---
+++ apps/web/src/components/auth/register-button.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import Link from "next/link"
+import { Button } from "../ui/button"
+import { HtmlHTMLAttributes } from "react"
+
+type RegisterButtonProps = {
+  children: React.ReactNode
+} & HtmlHTMLAttributes<HTMLButtonElement>
+
+export const RegisterButton = ({ children, ...props }: RegisterButtonProps) => {
+  return (
+    <Button
+      asChild
+      {...props}
+    >
+      <Link href="/auth/register">{children}</Link>
+    </Button>
+  )
+}
