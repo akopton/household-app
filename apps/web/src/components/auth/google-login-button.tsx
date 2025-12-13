7fde24bc85991e2907af980023e2c39ae7825b4b
Aleksander Kopton <kopton.olek@gmail.com>
2025-12-13 14:26:13 +0100

    feat: add Google login button

---
+++ apps/web/src/components/auth/google-login-button.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import { Button } from "../ui/button"
+import { FcGoogle } from "react-icons/fc"
+
+export const GoogleLoginButton = () => {
+  return (
+    <Button
+      asChild
+      variant="outline"
+      style={{ cursor: "pointer" }}
+    >
+      <Link href="/api/auth/signin/google">
+        <FcGoogle />
+        Google
+      </Link>
+    </Button>
+  )
+}
