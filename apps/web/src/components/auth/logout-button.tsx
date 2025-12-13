3fb1cb6c681c13eb51118584cc97f78adfd94a0c
Aleksander Kopton <kopton.olek@gmail.com>
2025-12-13 14:28:39 +0100

    feat: add logout button

---
+++ apps/web/src/components/auth/logout-button.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { signOut } from "next-auth/react"
+import { Button } from "../ui/button"
+
+type LogoutButtonProps = {
+  children: React.ReactNode
+} & React.ButtonHTMLAttributes<HTMLButtonElement> &
+  React.CSSProperties
+
+export const LogoutButton = ({ children, ...props }: LogoutButtonProps) => {
+  return (
+    <Button
+      onClick={() => {
+        signOut({ callbackUrl: "/" })
+      }}
+      {...props}
+    >
+      {children}
+    </Button>
+  )
+}
