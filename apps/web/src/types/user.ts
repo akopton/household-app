e509951e4e32d3997499832fde4cd09773764be4
Aleksander Kopton <kopton.olek@gmail.com>
2025-12-14 14:53:11 +0100

    feat: add type for User

---
+++ apps/web/src/types/user.ts
@@ -0,0 +1,8 @@
+export type TUser = {
+  id?: string
+  email?: string
+  username?: string | null
+  firstName?: string | null
+  lastName?: string | null
+  avatarUrl?: string
+}
--- packages/shared/src/schemas/user.ts
+++ packages/shared/src/schemas/user.ts
@@ -1,13 +1,15 @@
 import { z } from "zod"
 
 export const createUserSchema = z.object({
-  name: z.string().min(1).optional(),
+  firstName: z.string().min(1).optional(),
+  lastName: z.string().min(1).optional(),
   username: z.string().min(1).nullable().optional(),
   email: z.email(),
   password: z.string().min(8),
 })
 
 export const updateUserSchema = z.object({
-  name: z.string().min(1).optional(),
+  firstName: z.string().min(1).optional(),
+  lastName: z.string().min(1).optional(),
   username: z.string().min(1).nullable().optional(),
 })
