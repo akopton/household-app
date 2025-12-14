0aab1782c0c9f03ecca0466e354bf85dab9d10ea
Aleksander Kopton <kopton.olek@gmail.com>
2025-12-14 21:39:38 +0100

    feat: add user component for sidebar

---
+++ apps/web/src/components/layout/sidebar-user.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { BadgeCheck, Bell, ChevronsUpDown, LogOut } from "lucide-react"
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "../ui/sidebar"
+import { TUser } from "@/types/user"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu"
+import { signOut } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { UserAvatar } from "../user-profile/user-avatar"
+import { UserDetails } from "../user-profile/user-details"
+
+export const SidebarUser = ({ user }: { user: TUser }) => {
+  const router = useRouter()
+  const { isMobile } = useSidebar()
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground cursor-pointer"
+            >
+              <UserAvatar user={user} />
+              <UserDetails user={user} />
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <UserAvatar user={user} />
+                <UserDetails user={user} />
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onClick={() => router.push("/profile")}
+              >
+                <BadgeCheck />
+                Account
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="cursor-pointer">
+                <Bell />
+                Notifications
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => signOut()}
+              className="cursor-pointer"
+            >
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  )
+}
