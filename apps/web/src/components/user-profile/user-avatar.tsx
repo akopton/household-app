9aa0df6d8544608d293d7284c6bd336db1148c00
Aleksander Kopton <kopton.olek@gmail.com>
2025-12-15 19:07:06 +0100

    feat: refactor user avatar component

--- apps/web/src/components/user-profile/user-avatar.tsx
+++ apps/web/src/components/user-profile/user-avatar.tsx
@@ -5,79 +5,89 @@ import { ClassValue } from "clsx"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 import { Plus } from "lucide-react"
 import { useEffect, useState } from "react"
-import { TUser } from "@/types/user"
 import { Input } from "../ui/input"
 
 export const UserAvatar = ({
-  user,
+  url,
+  alt,
+  fallback,
   className,
-  editable,
+  editable = false,
 }: {
-  user?: TUser
+  url?: string
+  alt?: string
+  fallback?: string
   className?: ClassValue
   editable?: boolean
 }) => {
-  const [img, setImg] = useState<File | null | undefined>(null)
-  const [url, setUrl] = useState<string>("")
+  const [avatarUrl, setAvatarUrl] = useState<string | undefined>()
 
-  useEffect(() => {
-    fetch("/api/user/avatar")
-      .then((res) => res.json())
-      .then((data) => setUrl(data.avatarUrl))
-  }, [])
-
-  useEffect(() => {
+  const upload = (img: File | null | undefined) => {
     if (img) {
       const formData = new FormData()
       formData.append("file", img)
       fetch("/api/user/avatar", {
         method: "POST",
-        headers: { "Content-Type": "multipart/form-data" },
         body: formData,
       })
         .then((res) => res.json())
-        .then((data) => setUrl(data.avatarUrl))
+        .then((data) => {
+          setAvatarUrl(data.avatarUrl)
+          cookieStore.set("avatarUrl", data.avatarUrl)
+        })
     }
-  }, [img])
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    upload(file)
+  }
+
+  useEffect(() => {
+    setAvatarUrl(url)
+  }, [url])
 
   return (
-    <label htmlFor="avatar-upload">
+    <label
+      htmlFor={editable ? "avatar-upload" : "undefined"}
+      className={editable ? "cursor-pointer" : ""}
+    >
       <Avatar
         className={cn(
-          "relative h-8 w-8 rounded-lg cursor-pointer group overflow-visible",
+          "relative h-8 w-8 rounded-lg group overflow-visible",
           className
         )}
       >
         <AvatarImage
-          src={url}
-          alt={`${user?.firstName}-${user?.lastName}`}
+          src={avatarUrl || undefined}
+          alt={alt}
+          className={cn("rounded-lg", className)}
         />
 
-        <AvatarFallback className="rounded-lg">
-          {user?.firstName?.[0]}
-          {user?.lastName?.[0]}
-        </AvatarFallback>
+        <AvatarFallback className="rounded-lg">{fallback}</AvatarFallback>
 
         {editable && (
           <div
             className="
           absolute -right-1 -bottom-1
           flex items-center justify-center
-          size-4 rounded-full
+          size-8 rounded-full
           bg-primary text-primary-foreground
         "
           >
-            <Plus className="size-3" />
+            <Plus className="size-6" />
           </div>
         )}
       </Avatar>
-      <Input
-        id="avatar-upload"
-        type="file"
-        accept="image/*"
-        className="hidden"
-        onChange={(e) => setImg(e.target.files?.[0])}
-      />
+      {editable && (
+        <Input
+          id="avatar-upload"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          onChange={handleChange}
+        />
+      )}
     </label>
   )
 }
