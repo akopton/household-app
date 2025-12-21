b827c389c95b15dda4c04d18c0e8b9f942b17e96
Aleksander Kopton <kopton.olek@gmail.com>
2025-12-21 22:31:20 +0100

    feat: add grid on dashboard page with tasks card

--- apps/web/src/app/[locale]/(app)/dashboard/page.tsx
+++ apps/web/src/app/[locale]/(app)/dashboard/page.tsx
@@ -1,17 +1,7 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Grid, GridElement } from "@/components/ui/grid"
+import { TasksDashboardCard } from "@/features/tasks"
 
 export default async function DashboardPage() {
-  const gridCards: {
-    header: string
-    content: string
-    footer: string
-    column: number
-    row: number
-    columnSpan: number
-    rowSpan: number
-  }[] = []
-
   return (
     <div className="@container/main flex flex-1 flex-col gap-2 h-full">
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 h-full">
@@ -21,20 +11,14 @@ export default async function DashboardPage() {
             rows={5}
             gap={5}
           >
-            {gridCards.map((card, index) => (
-              <GridElement
-                column={card.column}
-                row={card.row}
-                columnSpan={card.columnSpan}
-                rowSpan={card.rowSpan}
-                key={index}
-              >
-                <Card className="h-full w-full">
-                  <CardHeader>{card.header}</CardHeader>
-                  <CardContent>{card.content}</CardContent>
-                </Card>
-              </GridElement>
-            ))}
+            <GridElement
+              column={1}
+              row={1}
+              columnSpan={2}
+              rowSpan={2}
+            >
+              <TasksDashboardCard />
+            </GridElement>
           </Grid>
         </div>
       </div>
