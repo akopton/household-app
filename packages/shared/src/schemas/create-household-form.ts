09a4ab97d1dad19395d43283f3796254de9fdb2b
Aleksander Kopton <kopton.olek@gmail.com>
2025-12-22 15:25:47 +0100

    fix: delete comments in create household form schema

--- packages/shared/src/schemas/create-household-form.ts
+++ packages/shared/src/schemas/create-household-form.ts
@@ -1,16 +1,6 @@
 import { z } from "zod"
 
 export const createHouseholdFormSchema = z.object({
-  //   id               String            @id @default(cuid())
-  //   name             String
-  //   ownerId          String
-  //   owner            User              @relation(fields: [ownerId], references: [id])
-  //   createdAt        DateTime          @default(now())
-  //   updatedAt        DateTime          @updatedAt
-  //   tasks            Task[]
-  //   isDefault        Boolean           @default(false)
-  //   householdInvites HouseholdInvite[]
-
   name: z.string().trim().min(3),
   invitedUsers: z.array(z.email()).optional(),
 })
